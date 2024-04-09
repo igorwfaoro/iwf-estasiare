132ae50e81c251e5003518117f947319f408c082
Igor Wilian Faoro <igor.faoro17@gmail.com>
2024-04-09 11:41:56 -0300

    feat: metadata 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,38 @@ import { ReactNode } from 'react';
 import { Metadata } from 'next';
 import { locale } from '../util/locale';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import { COLORS } from '../util/colors';
+
+const meta = {
+  title: 'Estasiare | Construindo momentos mágicos!',
+  description:
+    'Acreditamos que cada momento especial merece ser celebrado de forma única e inesquecível. Com nossa plataforma intuitiva e flexível, você pode criar o evento dos seus sonhos, seja um casamento mágico, um aniversário emocionante ou qualquer ocasião especial que mereça ser comemorada.',
+  image: '/images/logo-profile.png'
+};
+
+export const metadata: Metadata = {
+  title: meta.title,
+  description: meta.description,
+  robots: 'index',
+  themeColor: COLORS.primary,
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+    images: meta.image,
+    type: 'website',
+    siteName: `Estasiare`
+  },
+  twitter: {
+    title: meta.title,
+    description: meta.description,
+    card: 'summary',
+    images: meta.image
+  }
+};
 
 dayjs.locale(locale.id);
 
 const { NEXT_PUBLIC_GTAG: GTAG } = process.env;
-
-export const metadata: Metadata = {
-  title: 'Estasiare'
-};
 
 interface RootLayoutProps {
   children: ReactNode;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,34 +4,6 @@ import HomeHeader from './components/HomeHeader/HomeHeader';
 import Events from './components/Events/Events';
 import About from './components/About/About';
 import Footer from './components/Footer/Footer';
-import { COLORS } from '../util/colors';
-
-const meta = {
-  title: 'Estasiare | Construindo momentos mágicos!',
-  description:
-    'Acreditamos que cada momento especial merece ser celebrado de forma única e inesquecível. Com nossa plataforma intuitiva e flexível, você pode criar o evento dos seus sonhos, seja um casamento mágico, um aniversário emocionante ou qualquer ocasião especial que mereça ser comemorada.',
-  image: '/images/logo-profile.png'
-};
-
-export const metadata: Metadata = {
-  title: meta.title,
-  description: meta.description,
-  robots: 'index',
-  themeColor: COLORS.primary,
-  openGraph: {
-    title: meta.title,
-    description: meta.description,
-    images: meta.image,
-    type: 'website',
-    siteName: `Estasiare`
-  },
-  twitter: {
-    title: meta.title,
-    description: meta.description,
-    card: 'summary',
-    images: meta.image
-  }
-};
 
 export default async function Home() {
   return (
